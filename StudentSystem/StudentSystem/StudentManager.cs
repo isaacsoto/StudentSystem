aabd1d62d3ab24dd0d27e4ae2676a2b86f958767
isaacsoto <isaacsotomar@gmail.com>
2020-02-01 21:50:22 -0400

    Adding StudentManager class to the project, and adding some of the methods that it will use

---
+++ StudentSystem/StudentSystem/StudentManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+    class StudentManager
+    {
+        private char[] LineFormatSeparators = { ',' };
+        private string DateFomat = "yyyyMMddHHmmss";
+        public Student CreateStudentFromLine(string Line)
+        {
+            string[] parameters = Line.Split(LineFormatSeparators);
+            return ProcessStudentLine(parameters);
+
+        }
+
+        private Student ProcessStudentLine(string[] parameters)
+        {
+            string name = parameters[1];
+            DateTime timeStamp = DateTime.ParseExact(parameters[3], DateFomat,
+                                System.Globalization.CultureInfo.InvariantCulture);
+
+            return null;
+        }
+
+        private Student CreateStudent(StudentType Type, string Name, PersonGender StudentGender, DateTime TimeStamp)
+        {
+            Student newStudent = new Student(Type, Name, StudentGender, TimeStamp);
+            return newStudent;
+        }
+    }
+}
