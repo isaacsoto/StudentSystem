f9e9272fe2a160490c4afc343a4cc694e822d083
isaacsoto <isaacsotomar@gmail.com>
2020-02-02 13:48:49 -0400

    Cleaning up, unused references

--- StudentSystem/StudentSystem/StudentType.cs
+++ StudentSystem/StudentSystem/StudentType.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentSystem
 {
     enum StudentType
     {
-        High,
-        Middle,
         Kinder,
+        Elementary
+        High,
         University
     }
 }
