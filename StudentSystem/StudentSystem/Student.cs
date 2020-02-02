44de3e98ec2c29e2fe5d3a4342e07a6be9f28a5f
isaacsoto <isaacsotomar@gmail.com>
2020-02-01 20:31:26 -0400

    Create the basic classes for manage students inside the systems

---
+++ StudentSystem/StudentSystem/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem
+{
+    class Student
+    {
+        private StudentType Type;
+        private String Name;
+        private PersonGender Gender; 
+        private DateTime TimeStamp;
+
+        public Student(StudentType Type, String Name, PersonGender Gender, DateTime TimeStamp) {
+            this.Type = Type;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.TimeStamp = TimeStamp;
+        }
+    }
+}
